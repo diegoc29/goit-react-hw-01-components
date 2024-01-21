af9d1b43f40bd3078d576482ad0dfbabf6e1045f
diego cuervo <dicuervo213@gmail.com>
2024-01-21 07:30:46 -0500

    modified

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -16,22 +16,5 @@ export const App = () => {
 };
 
 
-import React from 'react';
-import Profile from './Profile'; 
-import user from './user.json';
 
-const App = () => {
-  return (
-    <div>
-      <Profile
-        username={user.username}
-        tag={user.tag}
-        location={user.location}
-        avatar={user.avatar}
-        stats={user.stats}
-      />
-    </div>
-  );
-};
 
-export default App;
